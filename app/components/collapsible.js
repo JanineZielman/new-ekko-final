4eb6f03be5448d30f2925f011ca9b90c9151c1ab
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-04-13 10:56:41 +0200

    auto closing collapsible

--- app/components/collapsible.js
+++ app/components/collapsible.js
@@ -1,47 +1,34 @@
-import React, {useState, useEffect, useRef} from "react"
-// import { useRouter } from 'next/router'
-
+import React, {useEffect} from "react"
 import { useNavigate } from "react-router-dom";
 
 const Collapsible = ({children, trigger, open, slug}) => {
-	// const router = useRouter()
   const navigate = useNavigate();
-  const [openDiv, setOPenDiv] = useState("");
-	const contentRef = useRef();
 
 	useEffect(() => {
-    setOPenDiv(open);
 		setTimeout(function() {
 			if (document.getElementById(window.location.hash.replace('#', '') + "-content")){
-				document.getElementById(window.location.hash.replace('#', '') + "-content").style.height = document.getElementById(window.location.hash.replace('#', '') + "-content").scrollHeight + 'px';
+				document.getElementsByClassName('open')[0]?.classList.remove('open');
         document.getElementById(window.location.hash.replace('#', '') + "-content").classList.add('open');
 				window.scrollTo({
 					top: document.getElementById(window.location.hash.replace('#', ''))?.offsetTop,
 					behavior: 'smooth'
 				});
-				
+				console.log(document.getElementsByClassName('open')[0]);
 			}
 		}, 100);
 	})
 
 
 	const toggle = (e) => {
-		if (openDiv == false){
-      setOPenDiv(true);
-      navigate(`#${e.target.parentElement.id}`);
-		} else {
-      setOPenDiv(false);
-			navigate(`#${e.target.id}`);
-		}
+		navigate(`#${e.target.parentElement.id}`);
 	};
 
   return (
     <div className="Collapsible" id={slug}>
-			<div onClick={toggle} id={`${slug}`} className="trigger">
+			<div id={`${slug}`} onClick={toggle} className="trigger">
         <span>{trigger}</span>
       </div>
-			<div className={`content-parent ${openDiv ? "open" : "closed"}`} id={`${slug}-content`} ref={contentRef} style={openDiv ? { height: contentRef.current?.scrollHeight +
-  "px" } : { height: "0px" }}>
+			<div className={`content-parent`} id={`${slug}-content`} >
 				<div className='collapsible-content'>{children}</div>
 			</div>
 		</div>
