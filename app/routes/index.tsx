309ec84c0ad5e102f9c95b07a8a4e3c794b4066a
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-04-20 16:13:45 +0200

    home placeholder

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,88 +2,57 @@ import type { LoaderFunction, MetaFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 
 import Container from '~/components/container';
-import React, {useEffect} from 'react';
-import type { Navigation } from '~/service/data/global';
-import { getNavigation } from '~/service/data/global';
-import { Link } from '@remix-run/react';
+import Spacer from '~/components/spacer';
+import { fetchContentPage } from '~/service/data/contentPage';
+import type { PageEntry } from '~/service/data/contentPage';
+import Collapsible from '~/components/collapsible';
 
 
 export const loader: LoaderFunction = async () => {
-  const [navigation] = await Promise.all([
-    getNavigation()
+  const [about, ostre, ekko_festival_info] = await Promise.all([
+    fetchContentPage('about'),
+    fetchContentPage('ostre'),
+    fetchContentPage('ekko_festival_info')
   ]);
 
-  return { navigation };
+  return { about, ostre, ekko_festival_info };
 };
 
+export const meta: MetaFunction = ({ data }) => ({
+  title: (data as PageEntry).entry?.title,
+});
+
 export default function Index() {
-  const { navigation } = useLoaderData<{ navigation: Navigation }>();
-
-  const toggle = navigation.nodes?.filter(nav => nav.navHandle == 'toggle');
-
-  console.log(navigation)
-
-  useEffect(() => {
-    $(document).ready(() => {
-      $('.gravity').jGravity({ // jGravity works best when targeting the body
-        target: 'div#gravity', // Enter your target critera e.g. 'div, p, span', 'h2' or 'div#specificDiv', or even 'everything' to target everything in the body
-        ignoreClass: 'ignoreMe', // Specify if you would like to use an ignore class, and then specify the class
-        weight: 25, // Enter any number 1-100 ideally (25 is default), you can also use 'heavy' or 'light'
-        depth: 1, // Enter a value between 1-10 ideally (1 is default), this is used to prevent targeting structural divs or other items which may break layout in jGravity
-        drag: false // Decide if users can drag elements which have been effected by jGravity
-      });
-    });
-  }, [])
-
-  console.log(toggle.filter(word => word.url.includes('festival'))[0].url)
-
-
-  
+  const { about, ostre, ekko_festival_info } = useLoaderData<{ about: PageEntry; ostre: PageEntry, ekko_festival_info: PageEntry }>();
 
   return (
     <Container back={false}>
-      <div className="grid gravity-grid" id="wrapper">
-        {/* <Spacer number={72} border=""/> */}
-        <div className='gravity'>
-          {navigation.nodes.map((item, i) => {
-            return(
-              <>
-                {item.navName == 'Toggle' ?
-                  <div id="gravity" className={`gravity-item ${item.navName} ${item.title}`} 
-                  onClick={
-                    goTo => {
-                      window.location = `${item.url}`;
-                    }
-                  }>
-                    {item.title}
-                  </div>
-                :
-                  <>
-                    {item.navHandle == 'festival' ?
-                      <div id="gravity" className={`gravity-item ${item.navName} ${item.title}`} 
-                        onClick={
-                          goTo => {
-                            window.location = `${toggle.filter(word => word.url.includes('festival'))[0].url}/${item.url}`;
-                          }
-                        }>
-                          {item.title}
-                      </div>
-                    :
-                      <div id="gravity" className={`gravity-item ${item.navName} ${item.title}`} 
-                        onClick={
-                          goTo => {
-                            window.location = `${item.navHandle}/${item.url}`;
-                          }
-                        }>
-                          {item.title}
-                      </div>
-                    }
-                  </>
-                }
-              </>
-            )
-          })}
+      <div className="grid">
+        <Spacer number={60} border=""/>
+        <img className="main-image" src="/EKKO-landing-test-2.png" alt="" />
+      </div>
+      <Collapsible trigger={about.entry.title} open={false} slug={about.entry.slug}>
+        <div className='flex'>
+          <div className='contact' dangerouslySetInnerHTML={{ __html: about.entry.contact }}></div>
+          <div className='content' dangerouslySetInnerHTML={{ __html: about.entry.content }}></div>
         </div>
+      </Collapsible>
+      <Collapsible trigger={ostre.entry.title} open={false} slug={ostre.entry.slug}>
+        <div className='flex'>
+          <div className='contact' dangerouslySetInnerHTML={{ __html: ostre.entry.contact }}></div>
+          <div className='content' dangerouslySetInnerHTML={{ __html: ostre.entry.content }}></div>
+        </div>
+        <a className='show-all-button-simple' href="/ostre"><h2>Read more</h2></a>
+      </Collapsible>
+      <Collapsible trigger={ekko_festival_info.entry.title} open={false} slug={ekko_festival_info.entry.slug}>
+        <div className='flex'>
+          <div className='contact' dangerouslySetInnerHTML={{ __html: ekko_festival_info.entry.contact }}></div>
+          <div className='content' dangerouslySetInnerHTML={{ __html: ekko_festival_info.entry.content }}></div>
+        </div>
+        <a className='show-all-button-simple' href={`/festival/${ekko_festival_info.entry.linkedFestival[0]?.slug}`}><h2>Read more</h2></a>
+      </Collapsible>
+      <div className="grid">
+        <Spacer number={12} border=""/>
       </div>
     </Container>
   );
