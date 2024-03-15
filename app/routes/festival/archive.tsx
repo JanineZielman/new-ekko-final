9a71f30f5aeae1155b42d168eacf7787e81839c1
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-03-15 16:55:31 +0100

    archive update

--- app/routes/festival/archive.tsx
+++ app/routes/festival/archive.tsx
@@ -2,30 +2,40 @@ import type { LoaderFunction } from '@remix-run/node';
 import { Link, useLoaderData } from '@remix-run/react';
 import { fetchAllEvents } from '~/service/data/events';
 import type { AllEvents } from '~/service/data/events';
+import { fetchContentPage } from '~/service/data/contentPage';
+import type { PageEntry } from '~/service/data/contentPage';
+import Moment from 'moment';
 
 import Container from '~/components/container';
 import Spacer from '~/components/spacer';
 import ArchiveItem from '~/components/archiveItem';
 
-export const loader: LoaderFunction = () => {
-  return fetchAllEvents(999);
+
+export const loader: LoaderFunction = async () => {
+  const [events, archive] = await Promise.all([
+    fetchAllEvents(999),
+    fetchContentPage('archive')
+  ]);
+
+  return { events, archive };
 };
 
 export default function Archive() {
-  let { events } = useLoaderData<AllEvents>();
+  const { events, archive } = useLoaderData<{ events: AllEvents; archive: PageEntry}>();
 
   let filteredEvents: any[] = [];
   var currentTime = new Date();
   currentTime.setDate(currentTime.getDate() - 1);
 
-  filteredEvents = events.filter((item: any) => {
+  filteredEvents = events.events.filter((item: any) => {
     var itemDate = new Date(item.date);
     return itemDate.getTime() <= currentTime.getTime();
   });
 
   return (
     <Container back={false}>
-      <div className='fake-grid'>
+      <div className='fake-grid yellow'>
+        <h1 className='archive-title'>Archive</h1>
         {filteredEvents.map(event => (
           <>
             {event.type == 'festival' &&
@@ -35,6 +45,30 @@ export default function Archive() {
             }
           </>
         ))}
+        {archive.entry.pastEvents.map((item ,i) => {
+          return(
+            <>
+            {item.isFestival &&
+              <div className='agenda-item'>
+                <div className='time'>
+                  <div className='indicator'>Dato</div>
+                  <p className="cap">
+                    {Moment(item.date).format("dddd D.M.")}{item.dateEnd && ` - ${Moment(item.dateEnd)?.format("dddd D.M.")}`} 
+                  </p>
+                </div>
+                <div className="event-title">
+                  <div className='indicator'>Title</div>
+                  <p>{item.eventTitle}</p>
+                </div>
+                <div className='artists'>
+                  <div className='indicator'>Artister</div>
+                  <h3  dangerouslySetInnerHTML={{ __html: item.artists }}></h3>
+                </div>
+              </div>
+            }
+            </>
+          )
+        })}
       </div>
 
       <div className="grid">
